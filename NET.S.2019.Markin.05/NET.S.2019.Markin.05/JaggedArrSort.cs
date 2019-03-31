c8b9a807a064b65d6e825b2e95b58051d98332d0
Маркин_Максим <markin-maksim@mail.ru>
2019-03-31 22:21:34 +0300

    Done without tests

---
+++ NET.S.2019.Markin.05/NET.S.2019.Markin.05/JaggedArrSort.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Markin._05
+{
+    public static class JaggedArrSort
+    {
+        private delegate bool SortType(int[] arr1, int[] arr2);
+            
+        private static void Check(int[][] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException(nameof(arr));
+            if (arr.Length == 0)
+                throw new ArgumentException(nameof(arr));
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] == null)
+                    throw new ArgumentException();
+        }
+
+        private static bool SumInc(int[] arr1, int[] arr2)
+        {
+            if (arr1.Sum() > arr2.Sum())
+                return true;
+            else
+                return false;
+        }
+
+        private static bool SumDec(int[] arr1, int[] arr2)
+        {
+            if (arr1.Sum() < arr2.Sum())
+                return true;
+            else
+                return false;
+        }
+
+        private static bool MaxInc(int[] arr1, int[] arr2)
+        {
+            if (arr1.Max() > arr2.Max())
+                return true;
+            else
+                return false;
+        }
+
+        private static bool MaxDec(int[] arr1, int[] arr2)
+        {
+            if (arr1.Max() < arr2.Max())
+                return true;
+            else
+                return false;
+        }
+
+        private static bool MinInc(int[] arr1, int[] arr2)
+        {
+            if (arr1.Min() > arr2.Min())
+                return true;
+            else
+                return false;
+        }
+
+        private static bool MinDec(int[] arr1, int[] arr2)
+        {
+            if (arr1.Min() > arr2.Min())
+                return true;
+            else
+                return false;
+        }
+
+        public static void SortByMaxInc(int[][] jaggedArr)
+        {
+            Check(jaggedArr);
+            SortType sorttype = MaxInc;
+            Sort(jaggedArr, sorttype);
+        }
+
+        public static void SortByMaxDec(int[][] jaggedArr)
+        {
+            Check(jaggedArr);
+            SortType sorttype = MaxDec;
+            Sort(jaggedArr, sorttype);
+        }
+
+        public static void SortBySumInc(int[][] jaggedArr)
+        {
+            Check(jaggedArr);
+            SortType sorttype = SumInc;
+            Sort(jaggedArr, sorttype);
+        }
+
+        public static void SortBySumDec(int[][] jaggedArr)
+        {
+            Check(jaggedArr);
+            SortType sorttype = SumDec;
+            Sort(jaggedArr, sorttype);
+        }
+
+        public static void SortByMinInc(int[][] jaggedArr)
+        {
+            Check(jaggedArr);
+            SortType sorttype = MinInc;
+            Sort(jaggedArr, sorttype);
+        }
+
+        public static void SortByMinDec(int[][] jaggedArr)
+        {
+            Check(jaggedArr);
+            SortType sorttype = MinDec;
+            Sort(jaggedArr, sorttype);
+        }
+
+        private static void Sort(int[][] jaggedArr, SortType sortType)
+        {
+            int[] tempArr;
+            for (int i = 0; i < jaggedArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < jaggedArr.GetLength(0) - 1 - i; j++)
+                {
+                    if (sortType(jaggedArr[j], jaggedArr[j + 1]))
+                    {
+                        tempArr = jaggedArr[j];
+                        jaggedArr[j] = jaggedArr[j + 1];
+                        jaggedArr[j + 1] = tempArr;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NET.S.2019.Markin.05/NET.S.2019.Markin.05/Polynomial.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.S._2019.Markin._05
+{
+    public sealed class Polynomial
+    {
+        private double[] coefficient;
+
+        public Polynomial(params double[] coef)
+        {
+            if (coef == null)
+                throw new ArgumentNullException(nameof(coef));
+            if (coef.Length <= 1)
+                throw new ArgumentException(nameof(coef));
+
+            coefficient = coef;
+        }
+
+        private static double[] MaxMinLength(Polynomial pol1, Polynomial pol2, out int min)
+        {
+            double[] result;
+
+            if (pol1.coefficient.Length >= pol2.coefficient.Length)
+            {
+                result = new double[pol1.coefficient.Length];
+                min = pol2.coefficient.Length;
+            }
+            else
+            {
+                result = new double[pol2.coefficient.Length];
+                min = pol1.coefficient.Length;
+            }
+            return result;
+        }
+
+        public static Polynomial operator +(Polynomial pol1, Polynomial pol2)
+        {
+            double[] result;
+            int minLength;
+
+            result = MaxMinLength(pol1, pol2, out minLength);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                result[i] = pol1.coefficient[i] + pol2.coefficient[i];
+            }
+
+            return new Polynomial(result);
+        }
+
+
+        public static Polynomial operator -(Polynomial pol1, Polynomial pol2)
+        {
+            double[] result;
+            int minLength;
+
+            result = MaxMinLength(pol1, pol2, out minLength);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                result[i] = pol1.coefficient[i] - pol2.coefficient[i];
+            }
+
+            return new Polynomial(result);
+        }
+
+        public static Polynomial operator *(Polynomial pol1, Polynomial pol2)
+        {
+            double[] result;
+            int minLength;
+
+            result = MaxMinLength(pol1, pol2, out minLength);
+
+            for (int i = 0; i < minLength; i++)
+            {
+                result[i] = pol1.coefficient[i] * pol2.coefficient[i];
+            }
+
+            return new Polynomial(result);
+        }
+
+        private bool IsEqual(double[] first, double[] second)
+        {
+            if (first.Length != second.Length)
+                return false;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] != second[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var polynomial = obj as Polynomial;
+            return polynomial != null && IsEqual(this.coefficient, polynomial.coefficient);
+        }
+
+        public static bool operator ==(Polynomial first, Polynomial second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Polynomial first, Polynomial second)
+        {
+            return !first.Equals(second);
+        }
+
+        public override string ToString()
+        {
+            string resultStr = string.Empty;
+
+            for (int i = coefficient.Length - 1; i > 0; i--)
+            {
+                if (coefficient[i] != 0)
+                {
+                    if (coefficient[i] > 0 && resultStr != string.Empty)
+                        resultStr += "+";
+                    resultStr += (Convert.ToString(coefficient[i]) + "*x^" + Convert.ToString(i));
+                }
+            }
+
+            if (coefficient[0] >= 0 && resultStr != string.Empty)
+                resultStr += "+";
+            resultStr += Convert.ToString(coefficient[0]);
+
+            return resultStr;
+        }
+
+        public override int GetHashCode()
+        {
+
+            int hashcode = 17;
+
+            for (int i = 0; i < coefficient.Length; i++)
+            {
+                hashcode = (hashcode * 7) + coefficient[i].GetHashCode();
+            }
+
+            return hashcode;
+        }
+    }
+}
