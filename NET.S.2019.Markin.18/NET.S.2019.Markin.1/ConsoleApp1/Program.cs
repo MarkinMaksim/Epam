52e78e58da35ffaf4e5e184f9d7a8feda0f3466c
Маркин_Максим <markin-maksim@mail.ru>
2019-05-05 23:51:01 +0300

    Done Day 18

---
+++ NET.S.2019.Markin.18/NET.S.2019.Markin.1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NET.S._2019.Markin._18;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            URLTxtReader urls = new URLTxtReader("input.txt");
+            URLSerialize.SaveXml(URLParser.Parse(urls.URLs), "result.txt");
+        }
+    }
+}
